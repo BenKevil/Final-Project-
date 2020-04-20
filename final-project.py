c87f953dfcf75a8318b8b5b190602d6c393bb1cc
Ben Kevil <64022027+BenKevil@users.noreply.github.com>
2020-04-20 09:42:14 -0500

    Update final-project.py
    Added in the class system

--- final-project.py
+++ final-project.py
@@ -1,3 +1,13 @@
 ########################
 # Final Project code ###
 ########################
+
+class Room(object):
+    pass
+
+
+class Monster(object):
+    pass
+
+class Player(object):
+    pass
