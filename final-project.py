6529730cfb7b742311f5ac40039e8ea7d5905d12
Lep021 <62142944+Lep021@users.noreply.github.com>
2020-04-20 19:54:26 -0500

    Update final-project.py

--- final-project.py
+++ final-project.py
@@ -108,23 +108,36 @@ class Fiends(Monster):
 
   def Combat(self, b):
     battle = 0
-    origin = 0
-    moveset = "Enchanted Strike: 1", "Heal: 2", "None: 3"
+    sorigin = 0
+    dorigin = 0
+    strike = 0
     while(battle == 0):
+      action = "Attack: 1", "Defend: 2", "Magic: 3", "Flee: 4"
+      moveset = "Enchanted Strike: 1", "Heal: 2", "None: 3"
+      print "What Command will you like to do?" +str(action)+""
+      command = input()
+      if command == 2:
+        sorigin = self.strength
+        dorigin = self.defense
+        self.strength = 0
+        self.defense = self.defense * 2
+
+      if command == 3:
+        print "Which magic do you want to use" +str(moveset)+""
+        strike = input()
+        if strike == 1:
+          if self.mp > 6:
+            print ""+str(self.owner)+"'s magic is being added to its atk"
+            sorigin = self.strength
+            self.strength = self.strength + (self.magic - b.magicdefense)
+            self.mp = self.mp - 6
+          else:
+            print ""+str(self.owner)+"'s mp is too low"
+          if strike == 2:
+            print ""+str(self.owner)+"'s hp is being healed"
+            self.hp = self.hp + self.magic
+
       first = self.agility - b.agility
-      print "Which magic do you want to use" +str(moveset)+""
-      strike = input()
-      if strike == 1:
-        if self.mp > 6:
-          print ""+str(self.owner)+"'s magic is being added to its atk"
-          origin = self.strength
-          self.strength = self.strength + (self.magic - b.magicdefense)
-          self.mp = self.mp - 6
-        else:
-          print ""+str(self.owner)+"'s mp is too low"
-      if strike == 2:
-        print ""+str(self.owner)+"'s hp is being healed"
-        self.hp = self.hp + self.magic
       if first > 0:
         b.hp = b.hp - (self.strength - b.defense)
         print "Attacker Hit"
@@ -155,10 +168,11 @@ class Fiends(Monster):
             battle = 1
             print ""+str(b.owner)+" lose"
       
+      if command == 2:
+        self.defense = dorigin
+        self.strength = sorigin
       if strike == 1:
-        self.strength = origin
-      
-      
+        self.strength = sorigin
 
   def __str__(self):
     return Monster.__str__(self)
