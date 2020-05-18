9c8610c9287c9e85a32a9c737bd2c59c01cc74bf
Axzel-Seaor <62144255+Axzel-Seaor@users.noreply.github.com>
2020-05-18 02:00:39 -0500

    Delete MonsterClass.py

--- MonsterClass.py
+++
@@ -1,9 +0,0 @@
-from CombatClass import Combat
-
-class Monster(Combat):
-  def __init__(self, name, hp, atk, end, mag, res, agi, loot, exp):
-    Combat.__init__(self, name, hp, atk, end, mag, res, agi)
-    self.loot = loot
-    self.exp = exp
-      
-
