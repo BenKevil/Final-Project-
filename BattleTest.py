d4134bd955d3bb72f96f275d2f1ca6bee604c958
Axzel-Seaor <62144255+Axzel-Seaor@users.noreply.github.com>
2020-05-18 02:00:26 -0500

    Delete BattleTest.py

--- BattleTest.py
+++
@@ -1,9 +0,0 @@
-from PlayerClass import Player
-from MonsterClass import Monster
-from random import randint
-
-Gob = Monster("Goblin", 9, 6, 4, 5, 4, 7, "knife", 50)
-Hero = Player("boy", randint(14, 20), 10, 3, 12,\
-        3, randint(4, 8))
-
-Hero.battle(Gob)
